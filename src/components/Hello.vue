bd97bc9a486a487c39060b6fd93e7d294b25d59b
Corbin Jensen <cjensen@carbonhouse.com>
2017-05-27 10:56:44 -0400

    dev - added line to test tower

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
+      <p>Test of git</p>
       <p>Your Name: {{name}}</p>
       <input type="text" v-model="name" />
       <p :click="submitName">Submit</p>
